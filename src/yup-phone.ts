69bb16bb909e845736fd6eeade155e75d45c8c2c
nightness <joshguyette@yahoo.com>
2021-01-10 22:16:18 -0600

    Using .required('This field is required') is cleaner

--- src/yup-phone.ts
+++ src/yup-phone.ts
@@ -1,7 +1,7 @@
-import * as Yup from 'yup';
-import gPhoneNumber from 'google-libphonenumber';
+import * as Yup from 'yup'
+import gPhoneNumber from 'google-libphonenumber'
 
-const phoneUtil = gPhoneNumber.PhoneNumberUtil.getInstance();
+const phoneUtil = gPhoneNumber.PhoneNumberUtil.getInstance()
 
 declare module 'yup' {
   export interface StringSchema {
@@ -16,58 +16,65 @@ declare module 'yup' {
       countryCode?: string,
       strict?: boolean,
       errorMessage?: string
-    ): StringSchema;
+    ): StringSchema
   }
 }
 
-const YUP_PHONE_METHOD = 'phone';
-const CLDR_REGION_CODE_SIZE = 2;
+const YUP_PHONE_METHOD = 'phone'
+const CLDR_REGION_CODE_SIZE = 2
 
 const isValidCountryCode = (countryCode: any): boolean =>
   typeof countryCode === 'string' &&
-  countryCode.length === CLDR_REGION_CODE_SIZE;
+  countryCode.length === CLDR_REGION_CODE_SIZE
 
-Yup.addMethod(Yup.string, YUP_PHONE_METHOD, function yupPhone(
-  countryCode?: string,
-  strict: boolean = false,
-  errorMessage: string = ''
-) {
-  const errMsg =
-    typeof errorMessage === 'string' && errorMessage
-      ? errorMessage
-      : isValidCountryCode(countryCode)
-      ? `\${path} must be a valid phone number for region ${countryCode}`
-      : '${path} must be a valid phone number.';
-  // @ts-ignore
-  return this.test(YUP_PHONE_METHOD, errMsg, (value: string) => {
-    if (!isValidCountryCode(countryCode)) {
-      // if not valid countryCode, then set default country to India (IN)
-      countryCode = 'IN';
-      strict = false;
-    }
-
-    try {
-      const phoneNumber = phoneUtil.parseAndKeepRawInput(value, countryCode);
-
-      if (!phoneUtil.isPossibleNumber(phoneNumber)) {
-        return false;
+Yup.addMethod(
+  Yup.string,
+  YUP_PHONE_METHOD,
+  function yupPhone(
+    countryCode?: string,
+    strict: boolean = false,
+    errorMessage: string = ''
+  ) {
+    const errMsg =
+      typeof errorMessage === 'string' && errorMessage
+        ? errorMessage
+        : isValidCountryCode(countryCode)
+        ? `\${path} must be a valid phone number for region ${countryCode}`
+        : '${path} must be a valid phone number.'
+    // @ts-ignore
+    return this.test(YUP_PHONE_METHOD, errMsg, (value: string) => {
+      if (!isValidCountryCode(countryCode)) {
+        // if not valid countryCode, then set default country to India (IN)
+        countryCode = 'IN'
+        strict = false
       }
 
-      const regionCodeFromPhoneNumber = phoneUtil.getRegionCodeForNumber(
-        phoneNumber
-      );
+      // This is what .required() is for
+      if (!value || (typeof value == 'string' && value.length == 0)) return true
 
-      /* check if the countryCode provided should be used as
+      try {
+        const phoneNumber = phoneUtil.parseAndKeepRawInput(value, countryCode)
+
+        if (!phoneUtil.isPossibleNumber(phoneNumber)) {
+          return false
+        }
+
+        const regionCodeFromPhoneNumber = phoneUtil.getRegionCodeForNumber(
+          phoneNumber
+        )
+
+        /* check if the countryCode provided should be used as
        default country code or strictly followed
      */
-      return strict
-        ? phoneUtil.isValidNumberForRegion(phoneNumber, countryCode)
-        : phoneUtil.isValidNumberForRegion(
-            phoneNumber,
-            regionCodeFromPhoneNumber
-          );
-    } catch {
-      return false;
-    }
-  });
-});
+        return strict
+          ? phoneUtil.isValidNumberForRegion(phoneNumber, countryCode)
+          : phoneUtil.isValidNumberForRegion(
+              phoneNumber,
+              regionCodeFromPhoneNumber
+            )
+      } catch {
+        return false
+      }
+    })
+  }
+)
